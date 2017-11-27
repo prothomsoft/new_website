51851e457afd7e1eedb25b4d3849a8205c324109
Tomasz Prokop <tomasz.prokop@lumesse.com>
2017-11-27 16:29:48 +0100

    last one

--- components/sesja/sesjaMobile.js
+++ components/sesja/sesjaMobile.js
@@ -15,23 +15,23 @@ export default class SesjaMobile extends React.Component {
                         <h1 className="quote-title">SESJA ZDJĘCIOWA KRAKÓW</h1>
                     </header>
                     <p>Lądując na mojej stronie internetowej trafiliście do fotografa, który od zawsze proponuje, namawia, zachwala sesje zdjęciowe w Krakowie. Trafiliście do fotografa, który swoim portfolio udowadnia, że nie trzeba jechać na koniec świata by zrobić dobre zdjęcia ślubne. Kraków znany w zimie głównie ze smogu już wiosną zamienia się w piękne, bardzo chętnie odwiedzane przez turystów, pełne historii miasto. Fajnych miejsc na sesję zdjęciową nie musimy szukać długo.</p>
-                    <LazyLoad offsetVertical={300}><img src={'/static/hotel_qubus_krakow_sesja_zdjeciowa_portfolio.jpg'} /></LazyLoad>
+                    <LazyLoad offsetVertical={300}><img src={'/static/portfolio/hotel_qubus_krakow_sesja_zdjeciowa_portfolio.jpg'} /></LazyLoad>
                     <p>Na północ od Krakowa rozciągają się atrakcje turystyczne Jury Krakowsko-Częstochowskiej reprezentowane przez Ojcowski Park Narodowy, Zamek w Pieskowej Skale, Dwór w Tomaszowicach czy Zamek w Korzkwi. Zachód Krakowa to Zalew w Kryspinowie, Dolina Bolechowicka, Zamek w Przegorzałach, Kopiec Kościuszki i Skałki Twardowskiego. Południe to Park w Mogilanach, Kopiec Kraka i Park Bednarskiego. Wschód Krakowa natomiast to industrialne klimaty Nowej Huty oraz Muzeum Lotnictwa na Czyżynach.</p>
-                    <LazyLoad offsetVertical={300}><img src={'/static/sesja_slubna_w_plenerze_park_krajobrazowy_w_ojcowie_i_okolice_portfolio.jpg'} /></LazyLoad>
+                    <LazyLoad offsetVertical={300}><img src={'/static/portfolio/sesja_slubna_w_plenerze_park_krajobrazowy_w_ojcowie_i_okolice_portfolio.jpg'} /></LazyLoad>
                     <p>Centrum to kwintesencja Krakowa czyli Bulwary Wiślane, Kazimierz z ulicą Szeroką, Plac Nowy, kluby i restauracje reprezentowane przez Mleczarnię, Alchemię, Starą Zajezdnie i Studio Qulinarne. Stare Miasto z ulicą Jagiellońska, Wawel, Planty, Zaułek Czartoryskich, Opera Krakowska, Sukiennice i Rynek Główny, Browar Lubicz, Hotel Stary - wymieniać można długo. Przygotowując się do sesji zdjęciowej i szukając odpowiedzi na pytanie gdzie można zrobić foto sesję w krakowie, wpiszcie miejsca z powyższego paragrafu, a gwarantuję, że znajdziecie coś dla siebie.</p>
-                    <LazyLoad offsetVertical={300}><img src={'/static/sesja_narzeczenska_krakow_portfolio.jpg'} /></LazyLoad>
+                    <LazyLoad offsetVertical={300}><img src={'/static/portfolio/sesja_narzeczenska_krakow_portfolio.jpg'} /></LazyLoad>
                     <p>Sesja zdjęciowa w Krakowie to przede wszystkim dobry plan w układaniu którego bardzo chętnie Wam pomogę, konsultując logistykę miejsc, podpowiadając pory dnia, które będą najlepsze dla danej lokalizacji. Zdjęcia z pleneru mają być niezwykłe i niepowtarzalne dlatego warto poświęcić chwilę na znalezienie odpowiednich rekwizytów, które nadadzą sesji oryginalności. </p>
-                    <LazyLoad offsetVertical={300}><img src={'/static/sesja_zdjeciowa_dwor_tomaszowice_krakow_portfolio.jpg'} /></LazyLoad>
+                    <LazyLoad offsetVertical={300}><img src={'/static/portfolio/sesja_zdjeciowa_dwor_tomaszowice_krakow_portfolio.jpg'} /></LazyLoad>
                     <p>Warto też przygotować makijaż i fryzurę zbliżone do tego z dnia ślubu . Koniecznie należy też zabrać ze sobą dobry humor, gdyż jest to bardzo ale to bardzo ważne.</p>
-                    <LazyLoad offsetVertical={300}><img src={'/static/sesja_slubna_strbskie_pleso_i_popradzki_staw_slowacja_portfolio.jpg'} /></LazyLoad>
+                    <LazyLoad offsetVertical={300}><img src={'/static/portfolio/sesja_slubna_strbskie_pleso_i_popradzki_staw_slowacja_portfolio.jpg'} /></LazyLoad>
                     <p>Fotografia jest moją pasją od lat. Jak nie trudno zgadnąć skala kreatywności poczas sesji zdjęciowych sięga zenitu. Każdy wieczór poprzedzający sesję zdjęciową mija mi na analizowaniu moich i istniejacych już w internecie prac innych fotografów wykonanych w miejscu lub okolicach miejsca do którego się wybieramy.</p>
-                    <LazyLoad offsetVertical={300}><img src={'/static/beata_i_marcin_reportaz_slubny_mszana_dolna_hotel_gorski_raj_portfolio.jpg'} /></LazyLoad>
+                    <LazyLoad offsetVertical={300}><img src={'/static/portfolio/beata_i_marcin_reportaz_slubny_mszana_dolna_hotel_gorski_raj_portfolio.jpg'} /></LazyLoad>
                     <p>Budowanie kreatywności, wyszukiwanie szczerości i wyjątkowości w zdjęciach to ciągła praca. Wierzcie mi, że im dalej jestem, tym więcej pracy widzę jeszcze przed sobą. Każda kolejna wykonana sesja zdjęciowa buduje świadomość tego, co ma być w kadrze, jak tego czegoś szukać. Definiuje kadr, podpowiada co chcę pokazywać i jak chcę pokazywać.</p>
-                    <LazyLoad offsetVertical={300}><img src={'/static/anna_krzysztof_portfolio.jpg'} /></LazyLoad>
+                    <LazyLoad offsetVertical={300}><img src={'/static/portfolio/anna_krzysztof_portfolio.jpg'} /></LazyLoad>
                     <p>Trzy godziny fotografowania, najczęściej dwa lub trzy wybrane miejsca w Krakowie i okolicach, blisko pięćset zdjęć, z których starannie selekcjonuję siedemdziesiąt do obróbki graficznej. Emocje, emocje i jeszcze raz emocje. Zero ustawek i balast sprzętowy ograniczony do niezbędnego minimum. Osobiście czerpię prawdziwą przyjemność w poszukiwaniu prawdziwości w moich zdjęciach, odnajdywaniu kadrów, odkrywaniu miejsc dzięki magii światła po raz kolejny na nowo.</p>
-                    <LazyLoad offsetVertical={300}><img src={'/static/wesele_na_polu_golfowym_w_paczoltowicach_portfolio.jpg'} /></LazyLoad>
+                    <LazyLoad offsetVertical={300}><img src={'/static/portfolio/wesele_na_polu_golfowym_w_paczoltowicach_portfolio.jpg'} /></LazyLoad>
                     <p>Plener ślubny w Krakowie to najlepszy moment na zrobienie tych najpiękniejszych i najromantyczniejszych zdjęć. To chwila na oderwanie się od rzeczywistości i wcielenie w rolę rozluźnionego modela. Spotkajmy się, wspólnie znajdźmy chwilę, światło, kadr, zachowajmy je na zawsze. Serdecznie zapraszam.</p>
-                    <LazyLoad offsetVertical={300}><img src={'/static/wesele_na_polu_golfowym_w_paczoltowicach_portfolio.jpg'} /></LazyLoad>
+                    <LazyLoad offsetVertical={300}><img src={'/static/portfolio/wesele_na_polu_golfowym_w_paczoltowicach_portfolio.jpg'} /></LazyLoad>
                     <div className="spacer">&nbsp;</div>
                 </article>
 
@@ -40,13 +40,13 @@ export default class SesjaMobile extends React.Component {
                         <h1 className="quote-title">REFERENCJE</h1>
                     </header>
                     <p>Od samego początku współpraca z Tomkiem przebiegała idealnie :) Pełen pomysłów w głowie i zapału do pracy, a przy okazji napełniony dobrą energii, która dodała nam "powera" w tym ważnym dla nas dniu ! Błyskawiczna obsługa z pięknymi zdjęciami :) Jak tylko wybierać to zdecydowanie Tomka ! :) Dziękujemy i na pewno kiedyś jeszcze się zobaczymy. <Link href="/hotel-qubus-krakow-sesja-zdjeciowa">Ewelina i Kamil</Link>.</p>
-                    <LazyLoad offsetVertical={300}><img src={'/static/wesele_na_polu_golfowym_w_paczoltowicach_portfolio.jpg'} /></LazyLoad>
+                    <LazyLoad offsetVertical={300}><img src={'/static/portfolio/wesele_na_polu_golfowym_w_paczoltowicach_portfolio.jpg'} /></LazyLoad>
                     <p>Pełen profesjonalizm i zaangażowanie! Dyskretna i bardzo efektowna praca Tomka robi wrażenie tak jak Jego zdjęcia :) spokój i usmiech :) świetna współpraca! Dziękujemy Tomek. <Link href="/sylwia-i-bartek-sesja-poslubna-krakow">Sylwia i Bartek</Link>.</p>
-                    <LazyLoad offsetVertical={300}><img src={'/static/wesele_na_polu_golfowym_w_paczoltowicach_portfolio.jpg'} /></LazyLoad>
+                    <LazyLoad offsetVertical={300}><img src={'/static/portfolio/wesele_na_polu_golfowym_w_paczoltowicach_portfolio.jpg'} /></LazyLoad>
                     <p>Gorące podziękowania dla Tomka za wspaniałą pamiątkę z naszego wesela, które odbyło się 15 sierpnia tego roku. Współpraca, podejście do tematu, zaangażowanie na bardzo wysokim poziomie. Materiał z wesela otrzymaliśmy w ekspresowym terminie. Wszystkim serdecznie polecamy! <Link href="/patrycja-i-michal-sesja-narzeczenska-pieskowa-skala">Patrycja i Michał</Link>.</p>
-                    <LazyLoad offsetVertical={300}><img src={'/static/wesele_na_polu_golfowym_w_paczoltowicach_portfolio.jpg'} /></LazyLoad>
+                    <LazyLoad offsetVertical={300}><img src={'/static/portfolio/wesele_na_polu_golfowym_w_paczoltowicach_portfolio.jpg'} /></LazyLoad>
                     <p>Tomek jest świetnym fotografem. Każdy ważny moment jest uchwycony w magiczny sposób. Nie ma zdjęcia, które by sie powtarzało. Efekty mówią same za siebie, wystarczy tylko popatrzeć na bloga. Atmosfera, która stwarza jest rewelacyjna. Gorąco polecam. <Link href="/wesele-tarnow-dwor-bistuszowa-i-staropolski-klimat-slubny">Aleksandra i Przemek</Link>.</p>
-                    <LazyLoad offsetVertical={300}><img src={'/static/wesele_na_polu_golfowym_w_paczoltowicach_portfolio.jpg'} /></LazyLoad>
+                    <LazyLoad offsetVertical={300}><img src={'/static/portfolio/wesele_na_polu_golfowym_w_paczoltowicach_portfolio.jpg'} /></LazyLoad>
                     <p>Just an amazing photograph that understand what the clients want! Would give him a 10 if it was possible (not possible on Facebook). <Link href="/slub-cywilny-i-wesele-w-plenerze-witkowa-chata-krakow">Kinga i Julien</Link>.</p>
                     <div className="spacer">&nbsp;</div>
                 </article>
