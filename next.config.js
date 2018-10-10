9d0723ba51613d8dfab113695a6e8b1302142130
Tomasz Prokop <tomasz.prokop@lumesse.com>
2018-10-10 15:01:15 +0200

    palac goetzow

--- next.config.js
+++ next.config.js
@@ -23,6 +23,7 @@ module.exports = {
             "/blog/page/11": { page: "/blog", query: { page: "11" } },
             "/blog/page/12": { page: "/blog", query: { page: "12" } },
             "/tester": { page: "/tester" },
+            "/sesja-slubna-jesienia-palac-goetzow-okocimskich": { page: "/sesja-slubna-jesienia-palac-goetzow-okocimskich" },
             "/dom-weselny-euforia-myslenice-i-sloneczne-love": { page: "/dom-weselny-euforia-myslenice-i-sloneczne-love" },
             "/sala-weselna-helada-fotograf-slubny-oswiecim": { page: "/sala-weselna-helada-fotograf-slubny-oswiecim" },
             "/sala-bankietowa-triana-mogilany-reportaz-slubny-podstolice": { page: "/sala-bankietowa-triana-mogilany-reportaz-slubny-podstolice" },
