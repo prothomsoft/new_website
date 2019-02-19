9ca2adc71ad0e820dddfaa2a77fa8e5f2e2715e8
Tomasz Prokop <tomasz@99foto.pl>
2019-02-19 20:57:08 +0100

    latest

--- pages/strefa-klienta.js
+++ pages/strefa-klienta.js
@@ -4,17 +4,17 @@ export default class ClientPageComponent extends React.Component {
     render() {
         let clients = [
             {
-                image1: "slub_zima.jpg",
-                url1: "martamichal",
-                title1: "GOŚCINIEC NAŁĘŻE JAWOŻE<br/>MARTA i MICHAŁ",
+                image1: "sala_bankietowa_impresja_jordanow.jpg",
+                url1: "patrycjastaszek",
+                title1: "SALA BANKIETOWA IMPRESJA JORDANÓW<br/>PATRYCJA i STANISŁAW",
 
                 image2: "pomysl_na_sesje_slubna_zimowa_w_krakowie.jpg",
                 url2: "natalialukasz",
                 title2: "DOM WESELNY ARKADIA CZASŁAW<br/>NATALIA i ŁUKASZ",
 
-                image3: "hotel_nad_raba_bochnia.jpg",
-                url3: "adaarek",
-                title3: "HOTEL NAD RABĄ BOCHNIA<br/>ADRIANNA i ARKADIUSZ",
+                image3: "slub_zima.jpg",
+                url3: "martamichal",
+                title3: "GOŚCINIEC NAŁĘŻE JAWOŻE<br/>MARTA i MICHAŁ"                
             },
 
             {
@@ -22,9 +22,9 @@ export default class ClientPageComponent extends React.Component {
                 url1: "nataliadominik",
                 title1: "TIFFANY WOLA FILIPOWSKA<br/>NATALIA i DOMINIK",
 
-                image2: "karczma_pod_kogutkiem_krzywaczka_1.jpg",
-                url2: "ilonaadam",
-                title2: "KARCZMA POD KOGUTKIEM KRZYWACZKA<br/>ILONA i ADAM",
+                image2: "zielone_wzgorze_koniusza.jpg",
+                url2: "karolamati",
+                title2: "ZIELONE WZGÓRZE KONIUSZA<br/>KAROLINA i MATEUSZ",
 
                 image3: "dom_weselny_euforia_myslenice_1.jpg",
                 url3: "izaarek",
@@ -32,38 +32,39 @@ export default class ClientPageComponent extends React.Component {
             },
 
             {
-                image1: "kristina.jpg",
-                url1: "kristinaaleksandr",
-                title1: "DWÓR SIERAKÓW W DOBCZYCACH<br/>KRISTINA i ALEKSANDR",
+                image1: "hotel_nad_raba_bochnia.jpg",
+                url1: "adaarek",
+                title1: "HOTEL NAD RABĄ BOCHNIA<br/>ADRIANNA i ARKADIUSZ",
 
-                image2: "zielone_wzgorze_koniusza.jpg",
-                url2: "karolamati",
-                title2: "ZIELONE WZGÓRZE KONIUSZA<br/>KAROLINA i MATEUSZ",
-
+                image2: "sala_bankietowa_triana_mogilany.jpg",
+                url2: "paulina",
+                title2: "SALA BANKIETOWA TRIANA MOGILANY<br/>PAULINA i PAWEŁ",
+                
                 image3: "fotograf-slubny-magnolia-budzow.jpg",
                 url3: "kingamateusz",
                 title3: "RESTAURACJA MAGNOLIA BUDZÓW<br/>KINGA i MATEUSZ"
             },
 
             {
-                image1: "sala_bankietowa_triana_mogilany.jpg",
-                url1: "paulina",
-                title1: "SALA BANKIETOWA TRIANA MOGILANY<br/>PAULINA i PAWEŁ",
+                image1: "fotograf_slubny_krzeszowice_start.jpg",
+                url1: "olagrzegorz",
+                title1: "CZTERY PORY ROKU ALWERNIA<br/>ALEKSANDRA i GRZEGORZ",
+                
+                image2: "kosciol_makow_podhalanski.jpg",
+                url2: "olakuba",
+                title2: "KARCZMA U LIPY JORDANÓW<br/>ALEKSANDRA i JAKUB",
 
-                image2: "magda_grzegorz.jpg",
-                url2: "magdagrzegorz",
-                title2: "RESTAURACJA KOLIBA MURZASICHLE<br/>MAGDALENA i GRZEGORZ",
-
-                image3: "kosciol_makow_podhalanski.jpg",
-                url3: "olakuba",
-                title3: "KARCZMA U LIPY JORDANÓW<br/>ALEKSANDRA i JAKUB"
+                image3: "magda_grzegorz.jpg",
+                url3: "magdagrzegorz",
+                title3: "RESTAURACJA KOLIBA MURZASICHLE<br/>MAGDALENA i GRZEGORZ"
             },
 
             {
-                image1: "helada_oswiecim.jpg",
-                url1: "magdadamian",
-                title1: "SALA WESELNA HELADA OŚWIĘCIM<br/>MAGDALENA i DAMIAN",
 
+                image1: "kristina.jpg",
+                url1: "kristinaaleksandr",
+                title1: "DWÓR SIERAKÓW W DOBCZYCACH<br/>KRISTINA i ALEKSANDR",
+                
                 image2: "magdalena_lukasz_myslenice.jpg",
                 url2: "magdalukasz",
                 title2: "REPORTAŻ ŚLUBNY MYŚLENICE<br/>MAGDALENA i ŁUKASZ",
@@ -74,9 +75,9 @@ export default class ClientPageComponent extends React.Component {
             },
 
             {
-                image1: "fotograf_slubny_krzeszowice_start.jpg",
-                url1: "olagrzegorz",
-                title1: "CZTERY PORY ROKU ALWERNIA<br/>ALEKSANDRA i GRZEGORZ",
+                image1: "helada_oswiecim.jpg",
+                url1: "magdadamian",
+                title1: "SALA WESELNA HELADA OŚWIĘCIM<br/>MAGDALENA i DAMIAN",
 
                 image2: "sala_weselna_soprano_tarnow.jpg",
                 url2: "magdalenalukasz",
