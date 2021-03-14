48695d75293de6e512e63a514ddb91a4182e1e25
Tomasz Prokop <tomasz@99foto.pl>
2021-03-14 18:24:37 +0100

    latest

--- components/slub/slubDesktop.js
+++ components/slub/slubDesktop.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Link from "next/link";
-import Image from 'next/image';
+import Image from "next/image";
 
 export default class OfferDesktop extends React.Component {
     constructor() {
@@ -27,12 +27,12 @@ export default class OfferDesktop extends React.Component {
                         eksponować emocje. Kadr to wiedza, setki przeglądniętych zdjęć i budowane latami poczucie estetyki. Fotografia ślubna wykorzystuje te narzędzia tworząc opowieść o ludziach,
                         miejscach, wydarzeniach i emocjach.
                     </p>
-                    <Image alt="Miejsca, wydarzenia, emocje w fotografii ślubnej" src={"/static/portfolio/portfolio_0013.jpg"} width={1140} height={760}/>
+                    <Image alt="Miejsca, wydarzenia, emocje w fotografii ślubnej" src={"/static/portfolio/portfolio_0013.jpg"} width={1140} height={760} />
                     <p>
                         Każde zdjęcie ślubne zaczyna się od pomysłu. To zwykle konfrontacja pomiędzy powstałym w naszej głowie ideałem i możliwościami jakie stwarza nam zastana sytuacja. Sprzęt
                         fotograficzny jest na pewno ważny, ale nie przeceniajmy jego wartości. W dzisiejszych czasach, ludzie bardzo często posługują się doskonałymi aparatami i obiektywami.
                     </p>
-                    <Image alt="Ogromne możliwości w fotografii ślubnej" src={"/static/portfolio/portfolio_0001.jpg"} width={1140} height={760}/>
+                    <Image alt="Ogromne możliwości w fotografii ślubnej" src={"/static/portfolio/portfolio_0001.jpg"} width={1140} height={760} />
                     <p>
                         Pamiętajmy jednak, że osoba zajmująca się fotografią ślubną to osoba, która poświęciła setki godzin na odkrywanie nowych technik, doskonalenie warsztatu, podpatrywanie
                         mistrzów, próby i błędy, analizowanie sytuacji i wyciąganie wynikających z nich wniosków. Kilka lat bacznego obserwowania rynku fotografii ślubnej, pasja i upór w dążeniu do
@@ -46,7 +46,7 @@ export default class OfferDesktop extends React.Component {
                         sesji zdjęciowych są tylko i wyłącznie towarzyskie. Jestem fanem autentycznych emocji. Staram się nie prosić o uśmiech i cierpliwie czekam, aż przyjdzie on sam. W mojej pracy
                         fotografa ślubnego kieruję się planem, doświadczeniem, ograniczając przypadek do minimum.
                     </p>
-                    <Image alt="Autentyczne emocje w fotografii ślubnej" src={"/static/portfolio/portret_panny_mlodej.jpg"} width={1140} height={760}/>
+                    <Image alt="Autentyczne emocje w fotografii ślubnej" src={"/static/portfolio/portret_panny_mlodej.jpg"} width={1140} height={760} />
                     <p>
                         Serdecznie zapraszam do odwiedzenia{" "}
                         <Link href="/blog">
@@ -55,8 +55,7 @@ export default class OfferDesktop extends React.Component {
                         i obejrzenia pełnych reportaży ślubnych, sesji zdjęciowych, zdjęć pełnych kolorów i autentycznych emocji. Oglądając moje prace sami ocenicie czy moja definicja stylu w
                         fotografii ślubnej będzie odpowiadać Waszym preferencjom. Wspólnie znajdźmy chwilę, światło, kadr, zachowajmy je na zawsze.
                     </p>
-                    <Image alt="Zdjęcia pełne kolorów i autentycznych emocji" src={"/static/portfolio/portfolio_0021.jpg"} width={1140} height={760}/>
-                    
+                    <Image alt="Zdjęcia pełne kolorów i autentycznych emocji" src={"/static/portfolio/portfolio_0021.jpg"} width={1140} height={760} />
                 </article>
 
                 <article>
@@ -70,14 +69,20 @@ export default class OfferDesktop extends React.Component {
                         polecamy Tomasza - choć reportaże na Jego blogu są chyba wystarczającą rekomendacją :) Raz jeszcze dziękujemy Ci Tomku za piękne i szybko oddane zdjęcia, współpracę z Tobą
                         będziemy zawsze miło wspominać! <Link href="/sala-weselna-nedan-pcim-reportaz-slubny">Anna i Krzysztof</Link>.
                     </p>
-                    <Image alt="sala weselna Nedan Pcim" src={"/static/portfolio/portfolio_0015.jpg"} width={1140} height={760}/>
+                    <Image alt="sala weselna Nedan Pcim" src={"/static/portfolio/portfolio_0015.jpg"} width={1140} height={760} />
                     <p>
                         Tomek zajmuje się fotografią ślubną z pasją, robi świetne zdjęcia, a do tego zna sie na tej sztuce! Poza tym jest wspaniałym, uśmiechniętym człowiekiem i praca z nim to czysta
                         przyjemność :-) słyszałam, że najlepszy aparaciarz to taki, na którego nie zwraca się uwagi i Tomek taki jest. Cichaczem pstryka fotki i przez to są naturalne, a na plenerze
                         daje mnóstwo super pomysłów :-) dziękujemy Tomku za zdjęcia ślubne i wczorajszy plener (to był dla nas z Pawłem wspaniały prezent na rocznice) !!!{" "}
                         <Link href="/kasia-i-pawel-zimowa-sesja-slubna-w-gorach">Kasia i Paweł</Link>.
                     </p>
-                    <Image alt="wesele na polu golfowym w Paczółtowicach" src={"/static/portfolio/wesele_na_polu_golfowym_w_paczoltowicach_portfolio.jpg"} width={1140} height={760}/>
+
+                    <Image alt="samochód ślubny tesla" src={"/static/portfolio/martyna_slub_tesla.jpg"} width={1140} height={525} />
+                    <p>
+                        Serdecznie polecamy Tomka! Świetny, bardzo profesjonalny fotograf, a jednoczesnie przesympatyczny i zawsze uśmiechnięty człowiek. Zdjęcia gotowe w ekspresowym tempie, a każde z
+                        nich wyjątkowe. Pozostaną dla nas przepiękną pamiątką na zawsze! Tomku, to była prawdziwa przyjemność z Tobą współpracować! Bardzo dziękujemy za wszystko! Maria i Amaury
+                    </p>
+                    <Image alt="ślub w hotelu kasprowy zakopane" src={"/static/portfolio/patrycja_slub.jpg"} width={1140} height={761} />
                     <p>
                         W każdym calu zajebisty fotograf, a przede wszystkim człowiek!!! Polecam z czystym sumieniem. Jeżeli ktoś oczekuje pełnego zaangażowania, precyzji, kompetencji, sprytnego oka
                         oraz wielu wielu innych drobnych jak i większych zdolności od fotografa - to właśnie znalazł odpowiednią osobę! Jeżeli chcesz aby fotograf plątał Ci się pod nogami albo
@@ -96,7 +101,7 @@ export default class OfferDesktop extends React.Component {
                         Piękne zdjęcia i pełen profesjonalizm. Dziękujemy jeszcze raz za wspaniałą współprace. Szczerze polecam.{" "}
                         <Link href="/hotel-lenart-wieliczka-turkusowy-cadillac-de-ville">Monika i Mariusz</Link>.
                     </p>
-                    <Image alt="hotel margerita krakow, magiczny wystrój sali weselnej" src={"/static/portfolio/hotel_margerita_krakow_magiczny_wystroj_sali_weselnej_w_krakowie.jpg"} width={1140} height={760} />
+                    <Image alt="wesele na polu golfowym w Paczółtowicach" src={"/static/portfolio/wesele_na_polu_golfowym_w_paczoltowicach_portfolio.jpg"} width={1140} height={760} />
                     <p>
                         Serdecznie polecamy Tomka! To świetny, niezwykle profesjonalny fotograf, który jest zaangażowany w swoją pracę, a przy tym jest przesympatyczny i zawsze uśmiechnięty. Zdjęcia
                         otrzymaliśmy w ekspresowym tempie, na każdym z nich Tomek uchwycił piękne chwile z naszego dnia. Zdjęcia są przepiękną pamiątką! Tomku, to była przyjemność z Tobą
@@ -107,7 +112,7 @@ export default class OfferDesktop extends React.Component {
                         Tomku, dziękujemy ogromnie za przepiękną pamiątkę jaką mamy dzięki Tobie. Twoje zaangażowanie i ogrom pomysłów widać w każdym ujęciu. Nie mogliśmy wymarzyć sobie lepszego
                         fotografa. Polecamy wszystkim ! <Link href="/fotograf-slubny-krzeszowice-cztery-pory-roku-alwernia">Aleksandra i Grzegorz</Link>.
                     </p>
-                    <Image alt="sala weselna alexandra mszana dolna" src={"/static/portfolio/aleksandra_mszana_dolna.jpg"} width={1140} height={760} />
+                    <Image alt="sala weselna Alexandra Mszana Dolna" src={"/static/portfolio/aleksandra_mszana_dolna.jpg"} width={1140} height={760} />
                     <p>
                         Zaangażowany, baczny obserwator wydarzeń, profesjonalista z pasją i pomysłowością. Czego efektem są piękne zdjęcia! Dziękujemy i polecamy!{" "}
                         <Link href="/palac-goetza-brzesko-sluba-sesja-zdjeciowa">Magdalena i Łukasz</Link>.
