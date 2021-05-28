0b0cb59379bf3511a1db95ca8b88e2ac7b4d8eb4
Tomasz Prokop <tprokop@csod.com>
2021-05-28 12:33:03 +0200

    cloudinary goes with www

--- next.config.js
+++ next.config.js
@@ -168,7 +168,7 @@ module.exports = {
         accessKeyId: process.env.AWS_ACCESS_KEY_ID_MYAPP,
         secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY_MYAPP,
         region: process.env.AWS_REGION_MYAPP,
-        staticImagesPath: "https://res.cloudinary.com/cloud99fotopl/image/fetch/f_auto,q_auto/https://99foto.pl/static/"
+        staticImagesPath: "https://res.cloudinary.com/cloud99fotopl/image/fetch/f_auto,q_auto/https://www.99foto.pl/static/"
         //staticImagesPath: "https://99foto.pl/static/"
     },
     images: {
