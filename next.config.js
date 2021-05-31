0f49eb00dab323fef3ec30eee184a9c8443c9351
Tomasz Prokop <tprokop@csod.com>
2021-05-31 20:47:02 +0200

    no cloudinary

--- next.config.js
+++ next.config.js
@@ -168,12 +168,12 @@ module.exports = {
         accessKeyId: process.env.AWS_ACCESS_KEY_ID_MYAPP,
         secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY_MYAPP,
         region: process.env.AWS_REGION_MYAPP,
-        staticImagesPath: "https://res.cloudinary.com/cloud99fotopl/image/fetch/f_auto,q_auto/https://www.99foto.pl/static/"
-        //staticImagesPath: "https://99foto.pl/static/"
+        //staticImagesPath: "https://res.cloudinary.com/cloud99fotopl/image/fetch/f_auto,q_auto/https://www.99foto.pl/static/"
+        staticImagesPath: "https://99foto.pl/static/"
     },
     images: {
-        loader: "cloudinary",
-        path: "https://res.cloudinary.com/cloud99fotopl",
+        //loader: "cloudinary",
+        //path: "https://res.cloudinary.com/cloud99fotopl",
         deviceSizes: [640, 750, 828, 1080, 1140, 1920]
     }
 };
