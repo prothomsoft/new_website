e05081e348c08fe4d5e2bf2d843732b0af98d565
Tomasz Prokop <tprokop@csod.com>
2022-01-11 11:33:39 +0100

    og for linkedin

--- components/meta.js
+++ components/meta.js
@@ -8,7 +8,10 @@ const MetaComponent = ({ title, description, keywords, url }) => (
         <meta name="keywords" content={keywords} />
         <meta name="google-site-verification" content="Ao8IvTUt4_ca5KfEOHEbknJCJogkxdlWgjlPz1I7AMg" />
         <meta name="theme-color" content="#000000" />
+        <meta property="og:title" content={title}/>
         <meta property="og:image" content="https://99foto.pl/static/icons/99foto.jpg" />
+        <meta property="og:description" content={description} />
+        <meta property="og:url" content={url} />
         <link rel="author" href="https://plus.google.com/u/0/+TomaszProkop_99FOTO" />
         <link rel="publisher" href="https://plus.google.com/u/0/+TomaszProkop_99FOTO" />
         <link rel="canonical" href={url} />
